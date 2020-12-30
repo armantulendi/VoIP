76f3a0e1f98edae95921fc43a74c62637af99afb
armantulendi <54793232+armantulendi@users.noreply.github.com>
2020-12-30 17:02:22 +0600

    1
    change

--- src/main/java/com/example/example/domain/Role.java
+++ src/main/java/com/example/example/domain/Role.java
@@ -4,7 +4,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.GrantedAuthoritiesContainer;
 
 public enum Role implements GrantedAuthority {
-    USER;
+    USER,ADMIN;
     @Override
     public String getAuthority() {
         return name();
