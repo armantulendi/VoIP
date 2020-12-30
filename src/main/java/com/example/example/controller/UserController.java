ce13442e71f8d44e96b05193f705071df333aebb
armantulendi <54793232+armantulendi@users.noreply.github.com>
2020-12-30 17:09:02 +0600

    Update UserController.java

--- src/main/java/com/example/example/controller/UserController.java
+++ src/main/java/com/example/example/controller/UserController.java
@@ -1,4 +1,20 @@
 package com.example.example.controller;
 
+import com.example.example.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping
+    public String userList(){
+        return "userList";
+    }
+
 }
